9ecde6263d4531779449034821d72e9f24559527
Cristina Isabel Cañizales <cristina.canizales@salesforce.com>
2025-02-27 16:29:24 -0300

    chore: sdfghjkl

--- agent-chat/src/App.tsx
+++ agent-chat/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from 'react';
+import React, { useRef, useState } from 'react';
 import {
   MainContainer,
   ChatContainer,
@@ -9,9 +9,9 @@ import {
   TypingIndicator
 } from '@chatscope/chat-ui-kit-react';
 import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
-import { AgentPreview, AgentPreviewSendResponse } from '@salesforce/agents';
-import fs from 'fs';
-import path from 'path';
+// import { AgentPreview } from '@salesforce/agents';
+// import fs from 'fs';
+// import path from 'path';
 // import { tmpdir } from 'os';
 import salesforceLogo from './assets/salesforce.png';
 import './index.css';
@@ -28,58 +28,18 @@ const initialComment: Message = {
   timestamp: new Date()
 };
 
-interface AppProps {
-  readonly agent: AgentPreview;
-  readonly id: string;
-  readonly name: string;
-}
-const saveTranscriptsToFile = (
-  tempDir: string,
-  messages: Array<{ timestamp: Date; role: string; content: string }>,
-  responses: AgentPreviewSendResponse[]
-): void => {
-  if (!tempDir) return;
-  fs.mkdirSync(tempDir, { recursive: true });
-
-  const transcriptPath = path.join(tempDir, 'transcript.json');
-  fs.writeFileSync(transcriptPath, JSON.stringify(messages, null, 2));
-
-  const responsesPath = path.join(tempDir, 'responses.json');
-  fs.writeFileSync(responsesPath, JSON.stringify(responses, null, 2));
-};
-
-const App: React.FC<AppProps> = props => {
+const App: React.FC = () => {
   // Inside your component
   const inputRef = useRef<HTMLInputElement>(null);
   const [query, setQuery] = useState('');
   const [sendDisabled, setSendDisabled] = useState(false);
-  const [_sessionId, setSessionId] = useState('');
-  const [timestamp, _setTimestamp] = useState(new Date().getTime());
-  const [tempDir, setTempDir] = useState('');
   const [isThinking, setIsThinking] = useState(false);
   const [messages, setMessages] = useState<Message[]>([initialComment]);
-  const { agent, id, name } = props;
-  const [responses, _setResponses] = React.useState<AgentPreviewSendResponse[]>([]);
-  useEffect(() => {
-    const startSession = async (): Promise<void> => {
-      const session = await agent.start(id);
-      setSessionId(session.sessionId);
-      await sleep(1500);
-      setIsThinking(false);
-      setTempDir(path.join(__dirname, 'agent-preview', `${timestamp}-${session.sessionId}`));
-      setMessages([{ role: name, content: session.messages[0].message, timestamp: new Date() } as Message]);
-    };
+  // const vscode = window.acquireVsCodeApi();
 
-    void startSession();
-  }, []);
-  useEffect(() => {
-    saveTranscriptsToFile(tempDir, messages, responses);
-  }, [tempDir, messages, responses]);
-  const vscode = window.acquireVsCodeApi();
-
-  const sendMessage = (message: string) => {
-    vscode.postMessage({ type: 'NEW_PROMPT', payload: message });
-  };
+  // const sendMessage = (message: string) => {
+  //   vscode.postMessage({ type: 'NEW_PROMPT', payload: message });
+  // };
 
   const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
   const handleSend = async (content: string) => {
@@ -89,13 +49,6 @@ const App: React.FC<AppProps> = props => {
     setIsThinking(true);
     setMessages(prev => [...prev, { id: prev.length + 1, role: 'user', content: content, timestamp: new Date() }]);
     await sleep(1500);
-    // const response = await agent.send(sessionId, content);
-    // setResponses(prev => [...prev, response]);
-    // const message = response.messages[0].message;
-
-    // if (!message) {
-    //   throw new Error('Failed to send message');
-    // }
 
     setIsThinking(false);
     setMessages(prev => {
@@ -116,10 +69,8 @@ const App: React.FC<AppProps> = props => {
             { ...lastComment, id: prev.length + 1, role: 'system', content: 'We have a scuba class at 9:30 AM' }
           ];
     });
-    sendMessage(content);
+    // sendMessage(content);
     setSendDisabled(false);
-    // Add the agent's response to the chat
-    // setMessages(prev => [...prev, { role: name, content: message, timestamp: new Date() }]);
     // Ensure the input regains focus
     setTimeout(() => inputRef.current?.focus(), 0);
   };
