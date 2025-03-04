8d667494fb9e61fb69a710241de27c403785578c
Cristian Dominguez <cdominguez@salesforce.com>
2025-03-04 15:56:37 -0300

    feat: support opening agents in MPD projects

--- src/commands/openAgentInOrg.ts
+++ src/commands/openAgentInOrg.ts
@@ -1,27 +1,26 @@
 import * as vscode from 'vscode';
 import { Commands } from '../enums/commands';
 import { SfProject } from '@salesforce/core-bundle';
-import * as path from 'path';
+import { Agent } from '@salesforce/agents-bundle';
 import { sync } from 'cross-spawn';
 import { CoreExtensionService } from '../services/coreExtensionService';
 
 export const registerOpenAgentInOrgCommand = () => {
   return vscode.commands.registerCommand(Commands.openAgentInOrg, async () => {
-    // TODO: maybe an Agent.listLocal() or something similar in the library
     const telemetryService = CoreExtensionService.getTelemetryService();
     telemetryService.sendCommandEvent(Commands.openAgentInOrg);
     const project = SfProject.getInstance();
-    const defaultPath = project.getDefaultPackage().fullPath;
-    const agents = (
-      await vscode.workspace.fs.readDirectory(vscode.Uri.file(path.join(defaultPath, 'main', 'default', 'bots')))
-    ).map(f => f[0]);
-
+    const agents = await Agent.list(project);
+    if (agents.length === 0) {
+      vscode.window.showErrorMessage(`Could not find agents in the current project.`);
+      return;
+    }
     // we need to prompt the user which agent to ope
-    const agentName = await vscode.window.showQuickPick(agents, { title: 'Choose which Agent to open' });
+    const agentName = await vscode.window.showQuickPick(agents, { placeHolder: 'Agent name (type to search)' });
 
     if (!agentName) {
       telemetryService.sendException('no_agent_selected', 'No Agent selected');
-      throw new Error('No Agent selected');
+      return;
     }
 
     await vscode.window.withProgress(
