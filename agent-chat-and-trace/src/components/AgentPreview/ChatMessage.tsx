606f179ac7a87fd31f5158658a0a160da9e609fc
Willie Ruemmele <willieruemmele@gmail.com>
2025-08-11 11:06:48 -0600

    chore: remove thumbs up / thumbs down ux on hover

--- agent-chat-and-trace/src/components/AgentPreview/ChatMessage.tsx
+++ agent-chat-and-trace/src/components/AgentPreview/ChatMessage.tsx
@@ -2,9 +2,6 @@ import React from 'react';
 import './ChatMessage.css';
 import userIcon from '../../assets/user.svg';
 import agentIcon from '../../assets/agent.svg';
-import thumbsUpIcon from '../../assets/thumbs-up.svg';
-import thumbsDownIcon from '../../assets/thumbs-down.svg';
-import copyIcon from '../../assets/copy.svg';
 
 interface ChatMessageProps {
   type: 'user' | 'agent';
@@ -23,19 +20,6 @@ const ChatMessage: React.FC<ChatMessageProps> = ({ type, content }) => {
       </div>
       <div className="message-content-wrapper">
         <div className="message-content">{content}</div>
-        {type === 'agent' && (
-          <div className="message-actions">
-            <button className="action-button" title="Thumbs up">
-              <img src={thumbsUpIcon} alt="Thumbs up" className="action-icon" />
-            </button>
-            <button className="action-button" title="Thumbs down">
-              <img src={thumbsDownIcon} alt="Thumbs down" className="action-icon" />
-            </button>
-            <button className="action-button" title="Copy">
-              <img src={copyIcon} alt="Copy" className="action-icon" />
-            </button>
-          </div>
-        )}
       </div>
     </div>
   );
