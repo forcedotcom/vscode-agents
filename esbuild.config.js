921636515181e0656ed3813e5d2c2f7db76732d7
Marcelino Llano <mllano@salesforce.com>
2025-10-07 16:22:42 +0200

    fix: update .vscodeignore to include webview development files, modify esbuild configuration to disable bundling and enable sourcemaps, and clean up package.json by removing unused dependencies

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,21 +1,13 @@
 const { build } = require('esbuild');
-const esbuildPluginPino = require('esbuild-plugin-pino');
 
 (async () => {
   await build({
-    bundle: true,
+    bundle: false,
     entryPoints: ['./src/extension.ts'],
     external: ['vscode'],
     format: 'cjs',
-    keepNames: true,
-    loader: { '.node': 'file' },
-    logOverride: {
-      'unsupported-dynamic-import': 'error'
-    },
-    minify: true,
     outdir: 'dist',
     platform: 'node',
-    plugins: [esbuildPluginPino({ transports: ['pino-pretty'] })],
-    supported: { 'dynamic-import': false }
+    sourcemap: true
   });
 })();
