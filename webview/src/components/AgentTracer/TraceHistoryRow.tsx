3eca92ee04482e60004139b1311de33a18c86cbe
Marcelino Llano <mllano@salesforce.com>
2026-02-06 23:14:36 +0100

    feat: add copy IDs button to trace row header

--- webview/src/components/AgentTracer/TraceHistoryRow.tsx
+++ webview/src/components/AgentTracer/TraceHistoryRow.tsx
@@ -69,6 +69,12 @@ export const TraceHistoryRow: React.FC<TraceHistoryRowProps> = ({
     onOpenJson();
   };
 
+  const handleCopyIds = (e: React.MouseEvent) => {
+    e.stopPropagation();
+    const text = `Session ID: ${traceData.sessionId}\nPlan ID: ${traceData.planId}`;
+    navigator.clipboard.writeText(text);
+  };
+
   return (
     <div className={`trace-history-row ${isExpanded ? 'trace-history-row--expanded' : ''}`}>
       <div className="trace-history-row__header-wrapper">
@@ -97,7 +103,18 @@ export const TraceHistoryRow: React.FC<TraceHistoryRowProps> = ({
         </button>
         <button
           type="button"
-          className="trace-history-row__json-icon"
+          className="trace-history-row__action-icon trace-history-row__copy-icon"
+          onClick={handleCopyIds}
+          aria-label="Copy IDs"
+          data-tooltip="Copy IDs"
+        >
+          <svg width="12" height="14" viewBox="0 0 12 14" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+            <path d="M1.97333 2.98667L2.98666 1.97333H8.37333L12 5.54667V12.96L10.9867 13.9733H2.98666L1.97333 12.96V2.98667ZM10.9867 5.97333L7.99999 2.98667H2.98666V12.96H10.9867V5.97333ZM0.959994 -9.53674e-07L-5.72205e-06 0.959999V10.9867L0.959994 12V0.959999H7.41333L6.39999 -9.53674e-07H0.959994Z" />
+          </svg>
+        </button>
+        <button
+          type="button"
+          className="trace-history-row__action-icon trace-history-row__json-icon"
           onClick={handleJsonClick}
           aria-label="Open JSON"
           data-tooltip="Open JSON"
